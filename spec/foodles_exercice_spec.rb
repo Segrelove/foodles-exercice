f1c2627c5b3a55c8fe0f1a05e753bb24cf9c4410
Segrelove <mlesegret@gmail.com>
2020-11-26 13:56:25 +0100

    adapted tests

--- spec/foodles_exercice_spec.rb
+++ spec/foodles_exercice_spec.rb
@@ -29,36 +29,30 @@ describe '#word_frequency' do
   end
 
   context 'it should return the right array' do
-    it 'should return the correct number of array' do 
-      sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-      expect(word_frequency(sentence, 3)).to eq([["zblah", 3], ["foo", 2], ["bar", 2]])
+    it 'should return the correct array' do 
+      sentence = "baz bar foo foo zblah zblah zblah baz toto toto bar"
+      expect(word_frequency(sentence, 10)).to eq([["zblah", 3], ["bar", 2], ["baz", 2], ["foo", 2], ["toto", 2]])
     end
 
-    it 'should return the correct number of array' do 
+    it 'should return the correct array' do 
       sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-      expect(word_frequency(sentence, 2)).to eq([["zblah", 3], ["foo", 2]])
+      expect(word_frequency(sentence, 3)).to eq([["zblah", 3], ["bar", 2], ["baz", 2]])
+    end
+
+    it 'should return the correct array' do 
+      sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
+      expect(word_frequency(sentence, 2)).to eq([["zblah", 3], ["bar", 2]])
     end
 
     it 'should return the correct number of array' do 
       sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
       expect(word_frequency(sentence, 1)).to eq([["zblah", 3]])
     end
-  end
 
-  context 'it shouldnt return the right array' do
-    it 'should return the correct number of array' do 
-      sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-      expect(word_frequency(sentence, 3)).not_to eq([["foo", 2], ["bar", 2]])
-    end
-
-    it 'should return the correct number of array' do 
-      sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-      expect(word_frequency(sentence, 2)).not_to eq([["zblah", 3], ["foo", 2], ["bar", 2]])
-    end
-
-    it 'should return the correct number of array' do 
-      sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-      expect(word_frequency(sentence, 1)).not_to eq([["zblah", 3], ["foo", 2]])
+    it 'should return nil if parameters is an empty string' do
+      sentence = " "
+      expect(word_frequency(sentence, 1)).to eq(nil)
+      expect(word_frequency(sentence, 10)).to eq(nil)
     end
   end
 end
